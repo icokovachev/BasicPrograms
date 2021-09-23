3d325a0059c96e28bc24ab7190e2dab3307ba95c
Христо Ковачев <hkovachev01@gmail.com>
2021-09-23 20:32:59 +0300

    Hello, world! task

---
+++ Hello, World!/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello__World_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine($"Hello, {userInput}!", userInput);
+        }
+    }
+}
