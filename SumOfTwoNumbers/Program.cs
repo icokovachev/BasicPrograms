8012a770dc7a4b2507e3851e6a774a7db9ca11d4
Христо Ковачев <hkovachev01@gmail.com>
2021-09-23 20:39:22 +0300

    SumOfTwoNumbers

---
+++ SumOfTwoNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double userInput1, userInput2, sum;
+            Console.Write("Enter the first number: ");
+            userInput1 = double.Parse( Console.ReadLine());
+            Console.Write("Enter the socond number: ");
+            userInput2 = double.Parse( Console.ReadLine());
+
+            Console.WriteLine($"The sum of the two numbers is: {userInput1 + userInput2} ");
+        }
+    }
+}
