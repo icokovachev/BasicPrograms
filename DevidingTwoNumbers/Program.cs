6e068b8ee99d8795c7283adfa76d017b0c14f383
Христо Ковачев <hkovachev01@gmail.com>
2021-09-23 20:48:55 +0300

    DevidingTwoNumbers

---
+++ DevidingTwoNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DevidingTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double input1, input2;
+            Console.Write("Enter positive number: ");
+            do
+                input1 = double.Parse(Console.ReadLine());
+            while (input1 <= 0); ;
+            Console.Write("Enter other positive number: ");
+            do
+                input2 = double.Parse(Console.ReadLine());
+            while (input2 <= 0); ;
+
+            Console.WriteLine($"The result of the operation is {input1/input2}");
+        }
+    }
+}
