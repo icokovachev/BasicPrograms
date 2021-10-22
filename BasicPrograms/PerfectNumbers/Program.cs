a50df675aa3371496e113742ae5664e3086f8513
Hristo Kovachev <hkovachev01@gmail.com>
2021-10-23 02:37:16 +0300

    Adding PerfectNumbers

---
+++ BasicPrograms/PerfectNumbers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PerfectNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter max num for evaluation");
+            int userInput = int.Parse(Console.ReadLine());
+            int sumOfArray = 0;
+
+            List<int> numbers = new List<int>(), perfectNumbers = new List<int>();
+            
+            for (int i = 1; i < userInput; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        numbers.Add(j);
+                    }
+                }
+
+                for (int x = 0; x < numbers.Count; x++)
+                    sumOfArray += numbers[x];
+
+
+                if (sumOfArray == i)
+                {
+                    perfectNumbers.Add(i);
+                }
+
+                sumOfArray = 0;
+                numbers.Clear();
+
+            }
+
+            for (int i = 0; i < perfectNumbers.Count; i++)
+            {
+                Console.WriteLine(perfectNumbers[i]);
+            }
+        }
+    }
+}
